940a4223ab69e424ad72113c5fabd18269daa3cc
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-22 16:57:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 class Owner
   attr_accessor :pets, :name
-
+  attr_reader :species
   @@all = []
 
   def initialize(name)
     @pets = {fishes: [], cats: [], dogs: []}
     @@all << self
+    @species = "human"
   end
 
   def self.all
@@ -19,6 +20,10 @@ class Owner
   def self.reset_all
     @@all.clear
     @@all
+  end
+
+  def species
+
   end
 
 end
